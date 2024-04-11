538d3c015faa1eefb79719ffc1c401521516f80b
Aziz <ngwk.zz@outlook.com>
2024-04-11 18:21:47 +0300

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,6 +12,8 @@ patterns = {
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
@@ -21,6 +23,19 @@ def filter_datum(
     """
     get, replace = (patterns["get"], patterns["replace"])
     return re.sub(get(fields, separator), replace(redaction), message)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Creates a logger
+    """
+    logger = logging.getLogger("user_data")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger.addHandler(stream_handler)
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
