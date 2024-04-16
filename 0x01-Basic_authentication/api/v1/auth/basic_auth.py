7e3adabdee51179e7a046ab62e3fd690e01b6c3c
Aziz <ngwk.zz@outlook.com>
2024-04-16 17:34:06 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,23 @@
 A module to manage the API authentication
 """
 from .auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """
     Basic authentication class.
     """
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
