fe4fb6bf9ba7ab9e56d1852a93ca9243e6682eca
Aziz <ngwk.zz@outlook.com>
2024-04-16 16:46:03 +0300

    Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ A class to manage the API authentication
 """
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -14,7 +15,18 @@ class Auth:
         """
         Check if authentication is required for a given path
         """
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
