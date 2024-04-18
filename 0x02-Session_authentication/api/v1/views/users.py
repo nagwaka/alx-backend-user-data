e8662e4346b727c6c4c290562c4a27bc8139abb8
Aziz <ngwk.zz@outlook.com>
2024-04-18 11:56:54 +0300

    Update method view_one_user

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,11 +30,12 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == 'me':
         if request.current_user is None:
             abort(404)
-        return jsonify(request.current_user.to_json()), 200
+        else:
+            return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
-    return jsonify(user.to_json()), 200
+    return jsonify(user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
