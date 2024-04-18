5b7fb5ffff7d5588f88ef550477ae992538ce1d6
Aziz <ngwk.zz@outlook.com>
2024-04-18 15:17:30 +0300

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,4 +13,13 @@ class SessionAuth(Auth):
     """
     Class for Session authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session id for the user.
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
