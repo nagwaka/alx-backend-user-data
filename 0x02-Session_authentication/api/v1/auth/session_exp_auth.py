e835ca792cf0b0e76b62bb4876702178e4490f7e
Aziz <ngwk.zz@outlook.com>
2024-04-18 21:26:08 +0300

    Expiration

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Module for the API Session authentication with expiration
+"""
+import os
+from flask import request
+from datetime import datetime, timedelta
+
+from .session_auth import SessionAuth
+
+
+class SessionExpAuth(SessionAuth):
+    """
+    Class for Session authentication with expiration.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes a new SessionExpAuth instance.
+        """
+        super().__init__()
+        try:
+            self.session_duration = int(os.getenv('SESSION_DURATION', '0'))
+        except Exception:
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """
+        Creates and returns the session id for the user.
+        """
+        session_id = super().create_session(user_id)
+        if type(session_id) != str:
+            return None
+        self.user_id_by_session_id[session_id] = {
+            'user_id': user_id,
+            'created_at': datetime.now(),
+        }
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None) -> str:
+        """
+        Returns the User ID by requesting UserSession in
+        the database based on session_id
+        """
+        if session_id in self.user_id_by_session_id:
+            session_dict = self.user_id_by_session_id[session_id]
+            if self.session_duration <= 0:
+                return session_dict['user_id']
+            if 'created_at' not in session_dict:
+                return None
+            cur_time = datetime.now()
+            time_span = timedelta(seconds=self.session_duration)
+            exp_time = session_dict['created_at'] + time_span
+            if exp_time < cur_time:
+                return None
+            return session_dict['user_id']
