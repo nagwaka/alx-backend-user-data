a8c95d1ca6de0dac6bfa730327d3574d240b7813
Aziz <ngwk.zz@outlook.com>
2024-04-25 08:25:41 +0300

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Password authentication
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password.
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
